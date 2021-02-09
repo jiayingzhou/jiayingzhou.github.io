6c7bedd7edb6d1b5b79ac6101f1891108ac76d98
Yuchuan Huang <itisyuchuan@gmail.com>
2021-02-09 14:35:29 -0600

    fix typo

--- experiences.js
+++ experiences.js
@@ -49,8 +49,8 @@ var experiences = React.createElement(
         null,
         "Experiences"
     ),
-    React.createElement(Experience, { where: "University of Minnesota, Tiwn Cities", role: "Ph.D., Department of Computer Science & Engineering", when: "09/2020 - Present", location: "Minneapolis, MN USA" }),
-    React.createElement(Experience, { where: "University of Minnesota, Tiwn Cities", role: "M.A., Department of Geography, Environment & Society", when: "09/2017 - 08/2020", location: "Minneapolis, MN USA" }),
+    React.createElement(Experience, { where: "University of Minnesota, Twin Cities", role: "Ph.D., Department of Computer Science & Engineering", when: "09/2020 - Present", location: "Minneapolis, MN USA" }),
+    React.createElement(Experience, { where: "University of Minnesota, Twin Cities", role: "M.A., Department of Geography, Environment & Society", when: "09/2017 - 08/2020", location: "Minneapolis, MN USA" }),
     React.createElement(Experience, { where: "JD.com, Inc.", role: "Research & Algorithm Engineer Intern", when: "06/2019 - 11/2019", location: "Beijing, China" }),
     React.createElement(Experience, { where: "Wuhan University", role: "B.Eng., School of Remote Sensing and Information Engineering", when: "09/2013 - 06/2017", location: "Wuhan, China" })
 );
