017534ac93d72dcea587a02209221ef4d6e71278
Yuchuan Huang <itisyuchuan@gmail.com>
2021-06-24 16:35:16 -0500

    update header

--- header.js
+++ header.js
@@ -1,6 +1,6 @@
 var header = React.createElement(
     "div",
-    { "class": "container px-5 mt-5 py-5" },
+    { "class": "container mt-5 py-5" },
     React.createElement(
         "div",
         { "class": "row" },
@@ -9,14 +9,14 @@ var header = React.createElement(
             { "class": "col" },
             React.createElement(
                 "h2",
-                { "class": "my-header" },
+                null,
                 "I'm Yuchuan Huang."
             )
         ),
         React.createElement(
             "div",
-            { "class": "col-sm-2" },
-            React.createElement("img", { "class": "d-none d-sm-block profile", src: "resource/profile/1440x1440.png", alt: "profile" })
+            { "class": "col-2" },
+            React.createElement("img", { "class": "d-none d-sm-block my-profile", src: "resource/profile/1440x1440.png", alt: "profile" })
         )
     )
 );
--- src/header.js
+++ src/header.js
@@ -1,11 +1,11 @@
 const header =
-    <div class="container px-5 mt-5 py-5">
+    <div class="container mt-5 py-5">
         <div class="row">
             <div class="col">
-                <h2 class="my-header">I'm Yuchuan Huang.</h2>
+                <h2>I'm Yuchuan Huang.</h2>
             </div>
-            <div class="col-sm-2">
-                <img class="d-none d-sm-block profile" src="resource/profile/1440x1440.png" alt="profile"></img>
+            <div class="col-2">
+                <img class="d-none d-sm-block my-profile" src="resource/profile/1440x1440.png" alt="profile"></img>
             </div>
         </div>
     </div>;
