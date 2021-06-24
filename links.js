f482ab7fdc862944ef75dcdc554ac22964a2f7f8
Yuchuan Huang <itisyuchuan@gmail.com>
2021-06-24 16:33:13 -0500

    update links

--- links.js
+++ links.js
@@ -1,15 +1,10 @@
 var links = React.createElement(
     "div",
-    { "class": "container px-5", id: "quickLinks" },
+    { "class": "container", id: "quickLinks" },
     React.createElement(
         "a",
         { "data-bs-toggle": "collapse", href: "#collapseBio" },
         "[Bio]"
-    ),
-    React.createElement(
-        "a",
-        { "data-bs-toggle": "collapse", href: "#collapseContact" },
-        "[Contact]"
     ),
     React.createElement(
         "a",
@@ -33,41 +28,14 @@ var links = React.createElement(
     ),
     React.createElement(
         "div",
-        { "class": "accordion-group" },
+        { "class": "collapse", id: "collapseBio", "data-bs-parent": "#quickLinks" },
         React.createElement(
             "div",
-            { "class": "collapse", id: "collapseBio", "data-bs-parent": "#quickLinks" },
+            null,
             React.createElement(
                 "div",
-                null,
-                React.createElement(
-                    "div",
-                    { "class": "container alert alert-success" },
-                    "Yuchuan Huang is a PhD student in the Department of Computer Science & Engineering at University of Minnesota, Twin Cities. His research interests are in database management systems. Yuchuan is advised by Prof. Mohamed F. Mokbel."
-                )
-            )
-        ),
-        React.createElement(
-            "div",
-            { "class": "collapse", id: "collapseContact", "data-bs-parent": "#quickLinks" },
-            React.createElement(
-                "div",
-                null,
-                React.createElement(
-                    "div",
-                    { "class": "container alert alert-success" },
-                    React.createElement(
-                        "span",
-                        null,
-                        "Email: huan1531[at]umn[dot]edu"
-                    ),
-                    React.createElement("hr", null),
-                    "Kenneth H. Keller Hall",
-                    React.createElement("br", null),
-                    "200 Union Street SE",
-                    React.createElement("br", null),
-                    "Minneapolis, MN 55455"
-                )
+                { "class": "container alert alert-success" },
+                "Yuchuan Huang is a PhD student in the Department of Computer Science & Engineering at University of Minnesota, Twin Cities. His research interest is database management systems. Yuchuan is advised by Prof. Mohamed F. Mokbel."
             )
         )
     )
--- src/links.js
+++ src/links.js
@@ -1,36 +1,21 @@
 const links =
-    <div class="container px-5" id="quickLinks">
+    <div class="container" id="quickLinks">
         <a data-bs-toggle="collapse" href="#collapseBio">[Bio]</a>
-        <a data-bs-toggle="collapse" href="#collapseContact">[Contact]</a>
         <a href="">[CV]</a>
         <a href="https://github.com/yhuang-db">[GitHub]</a>
         <a href="https://scholar.google.com/citations?user=QLZJaXgAAAAJ&hl=en">[Google Scholar]</a>
         <a href="https://www.linkedin.com/in/yuchuan-huang-db/">[LinkedIn]</a>
 
-        <div class="accordion-group">
-            <div class="collapse" id="collapseBio" data-bs-parent="#quickLinks">
-                <div>
-                    <div class="container alert alert-success">
-                        Yuchuan Huang is a PhD student in the Department of Computer Science & Engineering at University of Minnesota, Twin Cities.
-                        His research interests are in database management systems.
-                        Yuchuan is advised by Prof. Mohamed F. Mokbel.
-                        </div>
-                </div>
-            </div>
-            <div class="collapse" id="collapseContact" data-bs-parent="#quickLinks">
-                <div>
-                    <div class="container alert alert-success">
-                        <span>Email: huan1531[at]umn[dot]edu</span>
-                        <hr />
-                            Kenneth H. Keller Hall<br />
-                            200 Union Street SE<br />
-                            Minneapolis, MN 55455
-                        </div>
+        <div class="collapse" id="collapseBio" data-bs-parent="#quickLinks">
+            <div>
+                <div class="container alert alert-success">
+                    Yuchuan Huang is a PhD student in the Department of Computer Science & Engineering at University of Minnesota, Twin Cities.
+                    His research interest is database management systems.
+                    Yuchuan is advised by Prof. Mohamed F. Mokbel.
                 </div>
             </div>
         </div>
     </div>;
-
 
 
 ReactDOM.render(
